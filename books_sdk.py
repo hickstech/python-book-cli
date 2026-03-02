25f1c36e9a772d5c0122a33ffbbc5f5eaab990e8
Dad Hicks <bradhicks@Dads-MacBook-Pro-M3-Max.local>
2026-03-02 16:18:35 -0600

    basic cli and sdk

--- books_sdk.py
+++ books_sdk.py
@@ -2,14 +2,70 @@ from book import Book
 import sqlite3
 import os.path
 
-l = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+db_path = os.path.join(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))), "books.db"
+)
 
-conn = sqlite3.connect(os.path.join(l, "books.db"))
 
-c = conn.cursor()
-
-c.execute("""CREATE TABLE if not exists books(
+def setup_db():
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        c.execute("""CREATE TABLE if not exists books(
             title TEXT,
             pages INTEGER
+            )
+        """)
+
+
+setup_db()
+
+
+def add_book(book):
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO books VALUES (?, ?)", (book.title, book.pages))
+        return c.lastrowid
+
+
+# add_book(Book("This is a test book 2", 69))
+
+
+def get_books():
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        result = c.execute("SELECT * FROM books").fetchall()
+        return [Book(book[0], book[1]) for book in result]
+
+
+def get_book_by_title(title):
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        result = c.execute("SELECT * FROM books WHERE title = ?", (title,)).fetchone()
+        if result:
+            return Book(result[0], result[1])
+        return None
+
+
+# data = get_book_by_title("This is a test book")
+# print(data)
+
+
+def delete_book_by_title(title):
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        result = c.execute("DELETE FROM books WHERE title = ?", (title,))
+        return c.rowcount
+
+
+def delete_book(book):
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        result = c.execute(
+            "DELETE FROM books WHERE title = ? AND pages = ?", (book.title, book.pages)
         )
-""")
+        return c.rowcount
+
+
+# delete_book_by_title("This is a test book")
+# my_book = get_book_by_title("This is a test book 2")
+# delete_book(my_book)
---
+++ main.py
@@ -0,0 +1,34 @@
+import books_sdk
+from book import Book
+
+
+def print_menu():
+    leave = False
+    print("""Choose a number:
+          1. Print all books
+          2. Add a book
+          3. Delete a book
+          0. Quit
+          """)
+
+    print("Welcome to the library!")
+    while not leave:
+        response = int(input())
+        if response == 1:
+            for book in books_sdk.get_books():
+                print(book)
+        elif response == 2:
+            title = input("Title? ")
+            page_count = input("Page count? ")
+            books_sdk.add_book(Book(title, page_count))
+        elif response == 3:
+            title = input("Title to delete? ")
+            page_count = input("Page count? ")
+            books_sdk.delete_book_by_title(title)
+        else:
+            print("Thank you for visiting!")
+            leave = True
+            break
+
+
+print_menu()
